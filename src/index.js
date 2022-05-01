2605c83c6b4c07e86f9376819677639f8c049c94
mukul sharma <mukul02015@gmail.com>
2022-05-01 12:27:39 +1000

    loading template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+// scroll bar
+import 'simplebar/src/simplebar.css';
+
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
+//
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ----------------------------------------------------------------------
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
+  document.getElementById('root')
+);
+
+// If you want to enable client cache, register instead.
 serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
