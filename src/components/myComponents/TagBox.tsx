308035e3bb289e54cb03579665bbfddae9bb0548
mukul sharma <mukulsharma@pop-os.localdomain>
2022-05-07 14:02:24 +1000

    invalid

--- src/components/myComponents/TagBox.tsx
+++ src/components/myComponents/TagBox.tsx
@@ -5,21 +5,33 @@ import { TypeOption } from './docs/data';
 import { ActionMeta, MultiValueGenericProps, OptionProps } from 'react-select';
 import Select, { components } from "react-select";
 import Icon from '../Iconify';
+import { borderColor } from '@mui/system';
+import { boolean } from 'yup';
+import { Container,Typography } from '@mui/material';
 
-const options = [
+const defaultOptions = [
   { value: "legal", label: "LEGAL"},
   { value: "medical_imaging", label: "MEDICAL IMAGING"},
   { value: "programming", label: "PROGRAMMING"},
   { value: "pharmacist", label: "PHARMACIST"},
   { value: "general", label: "GEN"},
 ];
+const createOption = (label: string) => ({
+  label,
+  value: label.toLowerCase().replace(/\W/g, ''),
+});
+interface State {
+  readonly isLoading: boolean;
+  readonly invalidInput: boolean;
+  readonly options: readonly TypeOption[];
+}
 
 
 const Option = (props: OptionProps<TypeOption>) => {
   return (
     // <Tooltip content={'Customise your option component!'} truncate>
       <components.Option {...props} >
-        <div style={{backgroundColor:"#F8F9FA",borderRadius: '20px', width: "max-content", padding:"5px"}}>
+        <div style={{backgroundColor:"#F8F9FA",borderRadius: '20px', width: "max-content", padding:"4px"}}>
           <Icon icon="ant-design:tag-filled" color="#6C757D" width='20px' height='15px' sx={1}/> {props.data.label} {'     '}
         </div>
       </components.Option>
@@ -36,40 +48,96 @@ const MultiValueLabel = (props: MultiValueGenericProps<TypeOption>) => {
     // {/* </Tooltip> */}
   );
 };
-export default class TagBox extends Component<{}> {
+export default class TagBox extends Component<{}, State> {
+  
+  state: State = {
+    isLoading: false,
+    invalidInput: false,
+    options: defaultOptions,
+  };
+
+  handleInputChange = (inputValue: string) => {
+    this.setState({invalidInput: false})
+  }
+
+  handleCreate = (inputValue: string) => {
+    this.setState({ isLoading: true });
+    setTimeout(() => {
+      if (/^[a-zA-Z ]*$/.test(inputValue)) {
+        const newOption = createOption(inputValue.toUpperCase());
+        console.log(newOption);
+        this.setState({
+          isLoading: false,
+          invalidInput: false,
+          options: [...defaultOptions, newOption],
+        });
+      }
+      else {
+        this.setState({
+          isLoading: false,
+          invalidInput: true
+        })
+      }
+      
+    }, 1000);
+  };
 
   render() {
+    const { isLoading, invalidInput, options } = this.state;
+
     return (
-      <CreatableSelect
+      <Container>
+        <CreatableSelect
 
-        components={{ MultiValueLabel, Option }}
-        styles={{
-          multiValue: (base) => ({
-            ...base,
-            backgroundColor: "#6C757D",
-            // border: `2px`,
-            borderRadius: '20px',
-            color: 'white'
-          }),
-          multiValueLabel: (base) => ({
-            ...base,
-            color: 'white'
-          }),
-          option: (base) => ({
-            ...base,
-            color: '#6C757D'
-          }),
-          
-        }}
+            components={{ MultiValueLabel, Option }}
+            styles={{
+              multiValue: (base) => ({
+                ...base,
+                backgroundColor: "#6C757D",
+                // border: `2px`,
+                borderRadius: '20px',
+                color: 'white'
+              }),
+              multiValueLabel: (base) => ({
+                ...base,
+                color: 'white'
+              }),
+              option: (base) => ({
+                ...base,
+                color: '#6C757D'
+              }),
+              multiValueRemove: (base) => ({
+                ...base,
+                borderRadius: '20px',
+              }),
+              container: (base) => ({
+                ...base,
+                backgroundColor: invalidInput?"red": "",
+                padding: 2,
+              })
+            }}
 
 
-        isClearable
-        isSearchable      
-        isMulti
-        closeMenuOnSelect={false}
-        options={options}  
-        placeholder="Search"
-      />
+            isClearable
+            isSearchable      
+            isMulti
+            isLoading={isLoading}
+            // closeMenuOnSelect={false}
+            options={options}  
+            onInputChange={this.handleInputChange}
+            onCreateOption={this.handleCreate}
+            placeholder="Search"
+
+        />
+        
+        { invalidInput && 
+          <Typography color={"red"}>
+              <Icon icon="bi:exclamation-diamond-fill" color="red" sx={1} height='15px'/> Only Alphabetical Tags Allowed!
+          </Typography>
+        }
+
+      </Container>
+
     );
   }
 }
