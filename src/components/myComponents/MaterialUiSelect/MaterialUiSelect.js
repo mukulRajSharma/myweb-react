0bb60b71e7b9ec17b8594c59950df1c3b873725f
mukul sharma <mukulsharma@pop-os.localdomain>
2022-05-11 20:10:19 +1000

    fix dropdown

--- src/components/myComponents/MaterialUiSelect/MaterialUiSelect.js
+++ src/components/myComponents/MaterialUiSelect/MaterialUiSelect.js
@@ -19,7 +19,8 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: '35%',
+      // width: '35%',
+      width: 250
     },
   },
 };
